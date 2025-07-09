8ac4cb7d4669c5c5ac058e44a5e3f213199cb812
MelaniaPicone <melania_picone@yahoo.it>
2025-07-09 21:31:42 +0200

    [add] js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const lamp = document.getElementById('lamp');
+
+const button = document.querySelector('.btn');
+
+const text = button.querySelector('.text');
+
+button.addEventListener('click', function() {
+
+  if (btn.classList.contains('btn-secondary')) {
+
+clickBtn.classList.add('btn-warning');
+clickBtn.classList.remove('btn-secondary');  }
+
+else {
+clickBtn.classList.add('btn-secondary');
+clickBtn.classList.remove('btn-warning');
+
+
+}
+
+
+
+
+} )
